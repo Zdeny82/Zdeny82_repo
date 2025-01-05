4cf0cd426607de388971ae1a6dd4786265a174e2
Zdeněk Štěpánek <stepanek.zdenek.82@gmail.com>
2025-01-05 15:44:13 +0100

    response 10

--- Weather_dash_API/AirQuality_DashAPI.py
+++ Weather_dash_API/AirQuality_DashAPI.py
@@ -350,23 +350,37 @@ app.layout = dbc.Container(
         ),
 
         # Tabulka
-        dbc.Row(
-            dbc.Col(
-                dash_table.DataTable(
-                    id="city-table",
-                    columns=[{"name": col, "id": col} for col in df_to_show.columns],
-                    data=[],
-                    
-                    style_cell={"textAlign": "left"}  # Zarovnání textu doleva
-                ),
-                style={
-                        "display": "flex",
-                        
-                        "justify-content": "center",  # Zarovnání obsahu na střed
-                    },
-                width=12,
-            )
-        )
+        # Tabulka
+dbc.Row(
+    [
+        # První část tabulky
+        dbc.Col(
+            dash_table.DataTable(
+                id="city-table-part1",
+                columns=[{"name": col, "id": col} for col in df_to_show.iloc[:, :int(len(df_to_show.columns)/2)].columns],
+                data=[],  # Data můžete dynamicky plnit přes callback
+                style_cell={"textAlign": "left", "whiteSpace": "normal"},  # Zarovnání textu a zalomení
+                style_table={"overflowX": "auto"},  # Povolit horizontální scroll
+            ),
+            xs=12, sm=12, md=6, lg=6, xl=6,  # Responsivní šířky sloupce
+        ),
+
+        # Druhá část tabulky
+        dbc.Col(
+            dash_table.DataTable(
+                id="city-table-part2",
+                columns=[{"name": col, "id": col} for col in df_to_show.iloc[:, int(len(df_to_show.columns)/2):].columns],
+                data=[],  # Data můžete dynamicky plnit přes callback
+                style_cell={"textAlign": "left", "whiteSpace": "normal"},  # Zarovnání textu a zalomení
+                style_table={"overflowX": "auto"},  # Povolit horizontální scroll
+            ),
+            xs=12, sm=12, md=6, lg=6, xl=6,  # Responsivní šířky sloupce
+        ),
+    ],
+    style={"display": "flex", "flex-wrap": "wrap", "justify-content": "center"},  # Zalomení na menších zařízeních
+    className="mb-4"  # Odsazení mezi řádky
+)
+
     
     ],
     fluid=True,
